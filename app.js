a8d4dce43a122451d72774c02e9484faf28a7112
Ivarr Vinter <ivarrvinter@gmail.com>
2023-12-20 15:45:12 +0330

    Mobile

---
+++ app.js
@@ -0,0 +1,6 @@
+// const menuIcon = document.getElementById('menuIcon');
+// const menuItems = document.querySelector('.menu-items');
+
+// menuIcon.addEventListener('click', () => {
+//     menuItems.classList.toggle('show-menu');
+// });
